ae073680664e047ca61b74e4ad6a69a38e648060
Tistou <tistou.blomberg@gmail.com>
2016-05-03 13:10:59 +0200

    ThingToPhotograph class with it's own HTTP-check to the Image recognition-API

--- SnapThat/app/src/main/java/com/example/umyhpuscdi/snapthat/ThingToPhotograph.java
+++ SnapThat/app/src/main/java/com/example/umyhpuscdi/snapthat/ThingToPhotograph.java
@@ -3,48 +3,112 @@ package com.example.umyhpuscdi.snapthat;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 /**
  * Created by umyhblomti on 2016-05-02.
  */
-public class ThingToPhotograph {
-
+public class ThingToPhotograph{
 
     private String mName;
+
     private boolean isPhotographed = false;
+    private boolean uploadedAndChecked = false;
     private boolean accepted = false;
-    private boolean hasBeenUploadedAndChecked = false;
-    private Uri mFilePath;
+    private String mFilePath;
 
     public ThingToPhotograph(String name){
         mName = name;
     }
 
-    public void setFilePath(Uri filePath){
-        mFilePath = filePath;
-        isPhotographed = true;
-    }
-
-    public boolean isPhotographed(){
+    public boolean isPhotographed() {
         return isPhotographed;
     }
 
-    public boolean hasBeenUploadedAndChecked(){
-        return hasBeenUploadedAndChecked;
+    public boolean isUploadedAndChecked() {
+        return uploadedAndChecked;
     }
 
-    public boolean isAccepted(){
+    public boolean isAccepted() {
         return accepted;
     }
 
-    /*
-    Runs an Asynctask to upload the file to ImageRecognition webbservice.
-    When done:
-    hasBeenUploadedAndChecked = true;
-    accepted is updated to a correct value;
-     */
-    public String checkIfPhotoIsAccepted(){
+    public String getmFilePath() {
+        return mFilePath;
+    }
 
-        return "photo uploaded and checked successfully!";
+    public void setmFilePath(String mFilePath) {
+        this.mFilePath = mFilePath;
+        isPhotographed = true;
+    }
+
+    /*
+    Creates an Asynctask to upload the picture.
+    When the asyntask is done, postExecutePicToWordPOST is run which
+    updates:
+    uploadedAndChecked
+    accepted
+     */
+    public void uploadAndCheck(){
+        if(isPhotographed) {
+            PicToWordAsyncTask asyncTask = new PicToWordAsyncTask();
+            asyncTask.execute(getFile());
+        }else {
+            Log.i("ThingToPhotograph", "picture hasn't been taken yet, supply a filepath with setmFilePath");
+        }
+    }
+
+    public File getFile(){
+        return new File(mFilePath);
+    }
+
+    private void onPostExecuteUploadAndCheck(String jsonString){
+        if(doesJsonContainWord(jsonString)){
+            accepted = true;
+        }else {
+            accepted = false;
+        }
+        uploadedAndChecked = true;
+    }
+
+    private boolean doesJsonContainWord(String jsonString){
+        return true;
+    }
+
+    private class PicToWordAsyncTask extends AsyncTask<File, Void, String>{
+
+        private static final String API_URL = "https://quasiris-image-recognition-automatic-picture-labeling-v1.p.mashape.com/classify_upload?plain=1";
+
+        @Override
+        protected String doInBackground(File... params) {
+            try {
+                HttpResponse<JsonNode> response = Unirest.post(API_URL)
+                        .header("X-Mashape-Key", "aTaq4JgPedmshFlJznk83KutM6hWp1mPGnejsn8lEIHLCFHR6q")
+                        .field("imagefile", params[0])
+                        .asJson();
+                return response.toString();
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String jsonString) {
+            onPostExecuteUploadAndCheck(jsonString);
+        }
     }
 }
